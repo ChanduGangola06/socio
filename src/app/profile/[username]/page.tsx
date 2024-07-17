27e0704055dbf1996bdcf4b21a839224fe4119a2
Chandu Gangola <chandugangola444@gmail.com>
2024-07-17 21:18:32 +0530

    working on feed post

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,15 @@
+
+const ProfilePage = () => {
+  return (
+    <div className="flex gap-6 pt-6">
+        <div className="hidden xl:block w-[20%]">
+        {/* <LeftMenu type="profile" /> */}
+      </div>
+
+
+    </div>
+
+  )
+}
+
+export default ProfilePage;
